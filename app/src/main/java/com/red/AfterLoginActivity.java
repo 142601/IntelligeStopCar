07c63d4f0965d78d1b3230ce140d746958ec5d81
red <2249284426@qq.com>
2019-05-29 23:03:46 +0800

    update .gitignore in app

--- app/src/main/java/com/red/AfterLoginActivity.java
+++ app/src/main/java/com/red/AfterLoginActivity.java
@@ -2,20 +2,26 @@ package com.red;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 public class AfterLoginActivity extends AppCompatActivity {
 
     private MapView mMapView = null;
     private BaiduMap mBaiduMap = null;
     private LocationClient mLocationClient =null;
+    private boolean firstLocation = true;
+    private String TAG = "AfterLoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +31,56 @@ public class AfterLoginActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
 
-        mBaiduMap.setMyLocationEnabled(true);
+        mBaiduMap.setMyLocationEnabled(true);//开启定位图层
 
         //定位初始化
         mLocationClient = new LocationClient(this);
 
         //通过LocationClientOption设置LocationClient相关参数
         LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);//是否需要地址信息
         option.setOpenGps(true); // 打开gps
         option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(1000);
-
+        option.setScanSpan(1000);//每一秒发起一次定位请求间隔
         //设置locationClientOption
         mLocationClient.setLocOption(option);
 
         //注册LocationListener监听器
-        MyLocationListener myLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myLocationListener);
+        mLocationClient.registerLocationListener(new BDAbstractLocationListener() {
+            @Override
+            public void onReceiveLocation(BDLocation bdLocation) {
+                // map view 销毁后不在处理新接收的位置
+                if (bdLocation == null || mMapView == null)
+                    return;
+
+                //获取经纬度
+                double latitude = bdLocation.getLatitude();    //获取纬度信息
+                double longitude = bdLocation.getLongitude();    //获取经度信息
+                Log.i(TAG,"经度:" + longitude + "," + "维度" + latitude);
+
+                // 构造定位数据
+                MyLocationData locData = new MyLocationData.Builder()
+                        .accuracy(bdLocation.getRadius())
+                        // 此处设置开发者获取到的方向信息，顺时针0-360
+                        .direction(100)
+                        .latitude(latitude)
+                        .longitude(longitude)
+                        .build();
+                // 设置定位数据
+                mBaiduMap.setMyLocationData(locData);
+
+                // 第一次定位时，将地图位置移动到当前位置
+                if (firstLocation)
+                {
+                    firstLocation = false;
+                    LatLng xy = new LatLng(latitude,
+                            longitude);
+                    MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(xy);
+                    mBaiduMap.animateMapStatus(status);
+                }
+            }
+        });
+
         //开启地图定位图层
         mLocationClient.start();
     }
@@ -67,21 +106,5 @@ public class AfterLoginActivity extends AppCompatActivity {
         mMapView.onDestroy();
         mMapView = null;
         super.onDestroy();
-    }
-
-    public class MyLocationListener extends BDAbstractLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            //mapView 销毁后不在处理新接收的位置
-            if (location == null || mMapView == null){
-                return;
-            }
-            MyLocationData locData = new MyLocationData.Builder()
-                    .accuracy(location.getRadius())
-                    // 此处设置开发者获取到的方向信息，顺时针0-360
-                    .direction(location.getDirection()).latitude(location.getLatitude())
-                    .longitude(location.getLongitude()).build();
-            mBaiduMap.setMyLocationData(locData);
-        }
     }
 }
